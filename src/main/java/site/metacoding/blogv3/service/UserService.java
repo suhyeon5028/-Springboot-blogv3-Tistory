c48008b2c826de74d7d98ffbaeeae3a8fadad18f
suhyeon <suhyeon5028@naver.com>
2022-04-13 15:09:44 +0900

    회원가입시 유저네임, 비밀번호 중복체크 완료

--- src/main/java/site/metacoding/blogv3/service/UserService.java
+++ src/main/java/site/metacoding/blogv3/service/UserService.java
@@ -1,5 +1,7 @@
 package site.metacoding.blogv3.service;
 
+import java.util.Optional;
+
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -7,6 +9,8 @@ import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 import site.metacoding.blogv3.domain.user.User;
 import site.metacoding.blogv3.domain.user.UserRepository;
+import site.metacoding.blogv3.handler.ex.CustomException;
+import site.metacoding.blogv3.web.dto.user.PasswordResetReqDto;
 
 @RequiredArgsConstructor
 @Service // IoC 등록
@@ -17,6 +21,25 @@ public class UserService {
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Transactional
+    public void 패스워드초기화(PasswordResetReqDto passwordResetReqDto) {
+        // 1. username, email이 같은 것이 있는지 체크 (DB)
+        Optional<User> userOp = userRepository.findByUsernameAndEmail(
+                passwordResetReqDto.getUsername(),
+                passwordResetReqDto.getEmail());
+
+        // 2. 같은 것이 있다면 DB password 초기화 - BCrypt 해시 - update 하기 (DB)
+        if (userOp.isPresent()) {
+            User userEntity = userOp.get(); // 영속화
+            String encPassword = bCryptPasswordEncoder.encode("9999");
+            userEntity.setPassword(encPassword);
+        } else {
+            throw new CustomException("해당 이메일이 존재하지 않습니다.");
+        }
+
+        // 3. 초기화된 비밀번호 이메일로 전송
+    } // 더티체킹 (update)
+
+    @Transactional
     public void 회원가입(User user) {
         String rawPassword = user.getPassword(); // 1234
         String encPassword = bCryptPasswordEncoder.encode(rawPassword); // 해쉬 알고리즘
@@ -25,4 +48,13 @@ public class UserService {
         userRepository.save(user);
     }
 
+    public boolean 유저네임중복체크(String username) {
+        Optional<User> userOp = userRepository.findByUsername(username);
+
+        if (userOp.isPresent()) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 }
